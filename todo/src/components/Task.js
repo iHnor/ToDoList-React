f1554fa71dcbc7eba97b8be7298221e168d06281
Ihor <igormykhno@gmail.com>
2021-11-24 14:24:53 +0200

    add "done-task" class if task is done

--- todo/src/components/Task.js
+++ todo/src/components/Task.js
@@ -4,6 +4,7 @@ import DescriptDelete from './DescriptDelete';
 
 const Task = ({ task, onDelete, clickCheckBox }) => {
     return (
+        <div className={task.done ? "done-task task" : "task"} id="1" >
             <HeaderOfTask task={task} clickCheckBox={clickCheckBox}/>
             <DescriptDelete task={task} onDelete={onDelete}/>
         </div>
